dac40f67407a7e610ce2c12b5df19fabf7b75a56
sjuyal <sjuyal.23@gmail.com>
2018-07-27 14:40:20 +0530

    Single Page Application Complete

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -21,13 +21,9 @@ class Main extends Component {
       comments: COMMENTS,
       promotions: PROMOTIONS,
       leaders: LEADERS,
-      selectedDish: null
     };
   }
 
-  onDishSelect(dishId){
-    this.setState({selectedDish: dishId});
-  }
 
   render() {
 
@@ -40,12 +36,21 @@ class Main extends Component {
             />
         );
     }
+
+    const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}/>
+        );
+    }
+
     return (
       <div className="App">
         <Header/>
         <Switch>
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={HomePage} component={() => <Menu dishes={this.state.dishes} />} />
+            <Route path="/menu/:dishId" component={DishWithId}/>
             <Route exact path='/contactus' component={Contact} />} />
             <Redirect to="/home" />
         </Switch>
